e21e234e1e3682ef1867d62ec19309db1e96b9b8
mildred-prog <mildylicious@gmail.com>
2025-03-19 21:40:06 +0000

    deploy to hereoku

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-qzxcbv7#cn^bo3$-+w&j4t8o5ekphn(x6l)+inz1hdq1utqa@6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
